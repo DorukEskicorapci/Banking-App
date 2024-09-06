7e94da5cfba1c21a5c7ed4087233c4ac810b5651
Doruk Eskicorapci <de295@rutgers.edu>
2024-09-06 00:29:53 -0400

    "returns after balance"

--- main.py
+++ main.py
@@ -1,49 +1,68 @@
 import sqlite3
 from sqlcommands import dbfunctions
-try:
-
-    sqliteConnection = sqlite3.connect('database.db')
-    cursor = sqliteConnection.cursor()
-    print('Database initialization successful')
-    
-    filecontrol = open("demofile.txt", "a")
-    
-    filecontrol.write(dbfunctions.return_select(cursor))
-    dbfunctions.select(cursor)
-    
 
 
-    '''
-    dbfunctions.insert(cursor, 'John Doe', 50, 100)
-    
 
-    f = open("demofile3.txt", "w")
-    f.write("Woops! I have deleted the content!")
-    f.close()
+def driver():
+    try:
 
-    "x" - Create - will create a file, returns an error if the file exist
+        sqliteConnection = sqlite3.connect('database.db')
+        cursor = sqliteConnection.cursor()
+        print('Database initialization successful')
+        
+        dbfunctions.insert(cursor, 'Doruk', 'sifre', 50, 100)
 
-    "a" - Append - will create a file if the specified file does not exist
+        filecontrol = open("demofile.txt", "a")
+        
+        filecontrol.write(dbfunctions.return_select(cursor))
+        dbfunctions.select(cursor)
 
-    "w" - Write - will create a file if the specified file does not exist
+        IDinput = int(input('Enter a ID: '))
+        userinput = input('Enter a name: ')
+        passwordinput = input('Enter a password: ')
 
-    "a" - Append - will append to the end of the file
+        check = dbfunctions.checkinput(cursor, IDinput, userinput, passwordinput)
 
-    "w" - Write - will overwrite any existing content
-    '''
+        if check == True:
+            print('User Access Granted')
+            dbfunctions.balance_return(cursor, IDinput, userinput, passwordinput)
+                  
 
-    sqliteConnection.commit()
-    # Close the cursor
-    cursor.close()
+        
+        
+
+
+
+        '''
+        dbfunctions.insert(cursor, 'John Doe', 50, 100)
+        
+
+        f = open("demofile3.txt", "w")
+        f.write("Woops! I have deleted the content!")
+        f.close()
+
+        "x" - Create - will create a file, returns an error if the file exist
+
+        "a" - Append - will create a file if the specified file does not exist
+
+        "w" - Write - will create a file if the specified file does not exist
+
+        "a" - Append - will append to the end of the file
+
+        "w" - Write - will overwrite any existing content
+        '''
+
+        sqliteConnection.commit()
+        # Close the cursor
+        cursor.close()
+
+        if sqliteConnection:
+            sqliteConnection.close()
+            print('Database connection closed')
  
-# Handle errors
-except sqlite3.Error as error:
-    print('Error occurred;', error )
- 
-# Close DB Connection irrespective of success
-# or failure
-finally:
-   
-    if sqliteConnection:
-        sqliteConnection.close()
-        print('Database connection closed')
+    # Handle errors
+    except sqlite3.Error as error:
+        print('Error occurred;', error )
+    
+
+driver()
--- sqlcommands.py
+++ sqlcommands.py
@@ -1,9 +1,9 @@
 import sqlite3
 
 class dbfunctions:
-    def insert(cursor, name, cost, afterbalance):
+    def insert(cursor, name, password, cost, afterbalance):
         
-        cursor.execute('''INSERT INTO BillingHistory (Name, Cost, AfterBalance) VALUES(?, ?, ?)''', (name, cost, afterbalance))
+        cursor.execute('''INSERT INTO BillingHistory (Name, Password, Cost, AfterBalance) VALUES(?, ?, ?,?)''', (name, password, cost, afterbalance))
         
     
     def select(cursor):
@@ -18,3 +18,24 @@ class dbfunctions:
         for row in cursor:
             printc += str(row)
         return printc
+    
+    def checkinput(cursor, IDinput, userinput, passwordinput):
+        checkname = False
+        checkpassword = False
+        checkid = False
+        check = False
+        cursor.execute('''SELECT * FROM BillingHistory WHERE TransactionID = ? AND Name = ? AND Password = ?''', (IDinput, userinput, passwordinput))
+        
+        for row in cursor:
+            if row[0] == IDinput and row[1] == userinput and row[2] == passwordinput:
+                check = True
+        return check
+
+    def balance_return(cursor, IDinput, userinput, passwordinput):
+        cursor.execute('''SELECT AfterBalance FROM BillingHistory WHERE TransactionID = ? AND Name = ? AND Password = ?''', (IDinput, userinput, passwordinput))
+        result = cursor.fetchone()
+        if result:
+            print('Dear', userinput, 'your balance after the transaction is:', result[0])
+
+        else:
+            print('No matching record found')
