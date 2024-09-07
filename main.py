31978dc5368c3720d1db39e5e2f9c1bee8dea082
Doruk Eskicorapci <de295@rutgers.edu>
2024-09-07 10:52:39 -0400

    This commit refactors the code in main.py and sqlcommands.py to use a new customer table and transaction history. The changes include: - Replacing the insert function with insertCustomer function in dbfunctions class to insert customer data into the Customers table. - Adding a new insertTransaction function in dbfunctions class to insert transaction data into the TransactionHistory table. - Adding an updateCustomer function in dbfunctions class to update the balance of a customer in the Customers table. - Modifying the select function in dbfunctions class to accept a table parameter and select data from the specified table. - Updating the checkinput function in dbfunctions class to check for customer data in the Customers table instead of the BillingHistory table. - Updating the balance_return function in dbfunctions class to retrieve the balance from the Customers table.

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ def driver():
         cursor = sqliteConnection.cursor()
         print('Database initialization successful')
         
-        dbfunctions.insert(cursor, 'Doruk', 'sifre', 50, 100)
+        dbfunctions.insertCustomer(cursor, 'Customers', 'Doruk', 'sifre', 100)
+        dbfunctions.insertCustomer(cursor, 'Customers','Doruk', 'sifre', 100)
+        sqliteConnection.commit()
 
-        filecontrol = open("demofile.txt", "a")
+        #filecontrol = open("demofile.txt", "a")
         
-        filecontrol.write(dbfunctions.return_select(cursor))
-        dbfunctions.select(cursor)
+        #filecontrol.write(dbfunctions.return_select(cursor))
+
+        dbfunctions.select(cursor, 'Customers')
 
         IDinput = int(input('Enter a ID: '))
         userinput = input('Enter a name: ')
@@ -24,8 +27,44 @@ def driver():
         check = dbfunctions.checkinput(cursor, IDinput, userinput, passwordinput)
 
         if check == True:
-            print('User Access Granted')
+            print('User Access Granted' + '\n')
+        else:
+            print('User Access Denied' + '\n' + 'Redirecting to main menu')
+            driver()
+            
+        
+        print('1. View your balance' + '\n' + '2. View your transaction history' + '\n' + '3. Make new transaction' + '\n' + '4. Delete your account' + '\n' + '5. Exit')
+        nextstep = int(input('What do you want to do: '))
+        print()
+        if nextstep == 1:
+            print()
             dbfunctions.balance_return(cursor, IDinput, userinput, passwordinput)
+        elif nextstep == 2:
+            print('Dear', userinput, 'your balance after the transaction is:', dbfunctions.select(cursor, 'TransactionHistory'))
+            dbfunctions.select(cursor, 'TransactionHistory')
+        elif nextstep == 3:
+            askcompany = input('Enter the company name: ')
+            askcost = int(input('Enter the cost: '))
+            dbfunctions.insertTransaction(cursor, 'TransactionHistory', IDinput, askcompany, askcost)
+            balance = dbfunctions.balance_return(cursor, IDinput, userinput, passwordinput) - askcost
+            
+            print("Your balance after the transaction is ", balance)
+            
+            #Update the balance
+            dbfunctions.updateCustomer(cursor, IDinput, balance)
+
+
+        elif nextstep == 4:
+            
+
+
+        print("Transaction successful")
+
+          
+            
+            
+            
+
                   
 
         
--- sqlcommands.py
+++ sqlcommands.py
@@ -1,20 +1,26 @@
 import sqlite3
 
 class dbfunctions:
-    def insert(cursor, name, password, cost, afterbalance):
-        
-        cursor.execute('''INSERT INTO BillingHistory (Name, Password, Cost, AfterBalance) VALUES(?, ?, ?,?)''', (name, password, cost, afterbalance))
-        
+    def insertCustomer(cursor, table, name, password, balance):
+            
+        cursor.execute('''INSERT INTO {} (Name, Password, Balance) VALUES(?, ?, ?)'''.format(table), (name, password, balance))
     
-    def select(cursor):
+    def insertTransaction(cursor, table, UserID, CompanyName, Cost):
+            
+        cursor.execute('''INSERT INTO {} (UserID, CompanyName, Cost) VALUES(?, ?, ?)'''.format(table), (UserID, CompanyName, Cost))
+        
+    def updateCustomer(cursor, CustomerID, balance):   
+        cursor.execute('''UPDATE Customers SET Balance = ? WHERE CustomerID = ?''', (balance, CustomerID)) 
 
-        cursor.execute('''SELECT * FROM BillingHistory''')
+    def select(cursor, table):
+
+        cursor.execute('''SELECT * FROM {}'''.format(table))
         for row in cursor:
             print(row)
 
     def return_select(cursor):
         printc = ''
-        cursor.execute('''SELECT * FROM BillingHistory''')
+        cursor.execute('''SELECT * FROM Customers''')
         for row in cursor:
             printc += str(row)
         return printc
@@ -24,7 +30,7 @@ class dbfunctions:
         checkpassword = False
         checkid = False
         check = False
-        cursor.execute('''SELECT * FROM BillingHistory WHERE TransactionID = ? AND Name = ? AND Password = ?''', (IDinput, userinput, passwordinput))
+        cursor.execute('''SELECT * FROM Customers WHERE CustomerID = ? AND Name = ? AND Password = ?''', (IDinput, userinput, passwordinput))
         
         for row in cursor:
             if row[0] == IDinput and row[1] == userinput and row[2] == passwordinput:
@@ -32,10 +38,12 @@ class dbfunctions:
         return check
 
     def balance_return(cursor, IDinput, userinput, passwordinput):
-        cursor.execute('''SELECT AfterBalance FROM BillingHistory WHERE TransactionID = ? AND Name = ? AND Password = ?''', (IDinput, userinput, passwordinput))
+        cursor.execute('''SELECT Balance FROM Customers WHERE CustomerID = ? AND Name = ? AND Password = ?''', (IDinput, userinput, passwordinput))
         result = cursor.fetchone()
         if result:
-            print('Dear', userinput, 'your balance after the transaction is:', result[0])
+            #print('Dear', userinput, 'your balance after the transaction is:', result[0])
+            return result[0]
+            #print('Dear', userinput, 'your balance is:', result[0])
 
         else:
             print('No matching record found')
